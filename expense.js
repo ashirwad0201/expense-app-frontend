0dde612c923bb579178f7e9eeea74b98a433f382
ashirwad0201 <ashirwad0201@gmail.com>
2024-02-17 11:47:23 +0530

    frontend

---
+++ expense.js
@@ -0,0 +1,17 @@
+function onSignUp(){
+    var username_=document.getElementById('id1').value;
+    var email_=document.getElementById('id2').value;
+    var password_=document.getElementById('id3').value;
+
+    let myObj={
+        username: username_,
+        email: email_,
+        password: password_
+    }
+    console.log(myObj)
+    if(username_!='' && email_!='' && password_!=''){
+        axios.post('http://localhost:5000/insert-user',myObj)
+        .then((res)=>console.log(res))
+        .catch(err=>console.log(err));
+    }
+}
